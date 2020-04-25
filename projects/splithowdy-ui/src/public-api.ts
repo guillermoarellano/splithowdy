cd4b11da2a2e23ea97f28f76d2f5623198788e60
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-04-25 16:11:31 -0500

    ng generate library project

---
+++ projects/splithowdy-ui/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of splithowdy-ui
+ */
+
+export * from './lib/splithowdy-ui.service';
+export * from './lib/splithowdy-ui.component';
+export * from './lib/splithowdy-ui.module';
