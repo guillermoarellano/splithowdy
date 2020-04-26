b6ed1683b4f024ccbf9f443901f7b221361d0295
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-04-26 12:39:36 -0500

    guidepost for src level library ng packagr

--- projects/splithowdy-ui/src/public-api.ts
+++ projects/splithowdy-ui/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of splithowdy-ui
  */
 
-export * from './lib/splithowdy-ui.service';
-export * from './lib/splithowdy-ui.component';
-export * from './lib/splithowdy-ui.module';
+export * from './lib/name';
+export * from './lib/time';
