136c8f770be47e5b9bc870e146de8f223bb312dd
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2020-05-25 17:08:06 -0500

    move test and public-api to root

---
+++ projects/splithowdy-ui/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of splithowdy-ui
+ */
+
+export * from './src/lib/name';
+export * from './src/lib/time';
--- projects/splithowdy-ui/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of splithowdy-ui
- */
-
-export * from './lib/name';
-export * from './lib/time';
